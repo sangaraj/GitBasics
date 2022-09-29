88a92d0156d27de91c63826a8787618fe78097bb
sangaraj <rohitkumars1992@gmail.com>
2022-09-29 18:22:32 +0530

    add second line in second program

--- SecondProgram.java
+++ SecondProgram.java
@@ -2,5 +2,6 @@ public class SecondProgram {
     public static void main(String[] args)
     {
         System.out.println("This should go in branchone");
+        System.out.println("Second time, this line should go to branch one");
     }
 }
