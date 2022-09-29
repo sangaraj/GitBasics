b4390acbdff70f4b6941afd3da90b28f15131491
sangaraj <rohitkumars1992@gmail.com>
2022-09-29 16:56:37 +0530

    added second line

--- FirstProgram.java
+++ FirstProgram.java
@@ -2,5 +2,6 @@ public class FirstProgram {
     public static void main(String[] args)
     {
         System.out.println("First line of code to be pushed to git");
+        System.out.println("This is second line");
     }
 }
